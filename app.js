38063c5b5aced0dd51b70f08d4e06deab3e167ce
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-06-20 22:33:44 +0100

    create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const PORT = 3000;
+
+const express = require("express");
+const app = express();
+
+//this essentially the same as the code in the file ../lect1/http.js but much simpler thanks to express
+app.get("/", (req, res) => {
+  res.end("hello");
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
