6e58f8d6f1cf73f0af09231a0b41ba139d91a25e
personal <hasinmahmood01@gmail.com>
2023-06-24 20:55:30 +0100

    refactor: try, catch for error handling

--- index.js
+++ index.js
@@ -23,29 +23,33 @@ app.post("/", async (req, res) => {
   const { object, number_of_objects, extra_info } = req.body;
   obj = JSON.stringify(object);
 
-  const { data, error } = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content: `You are an expert data creator. Given an example schema you return a valid array of n just JSON objects 
-          (where n is defined by the user). For each property of the passed in object give it a relavent value. 
-          For example, if a property is name its value could be 'bob roberts' and if there is another property called 
-          ID it could be given the value '34532' etc. Only return the JSON value and no other strings or information`,
-      },
-      {
-        role: "user",
-        content: `object: ${obj} number of objects: ${number_of_objects} extra context: ${extra_info}`,
-      },
-    ],
-  });
-  const response = data.choices[0].message.content;
-  //Sometimes some of the responses still include other info and I cant figure out why so this ensures that the response is JSON
-  const startIndex = response.indexOf("[");
-  const endIndex = response.lastIndexOf("]") + 1;
-  const json = response.slice(startIndex, endIndex);
+  try {
+    const { data } = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: `You are an expert data creator. Given an example schema you return a valid array of n just JSON objects 
+            (where n is defined by the user). For each property of the passed in object give it a relavent value. 
+            For example, if a property is name its value could be 'bob roberts' and if there is another property called 
+            ID it could be given the value '34532' etc. Only return the JSON value and no other strings or information`,
+        },
+        {
+          role: "user",
+          content: `object: ${obj} number of objects: ${number_of_objects} extra context: ${extra_info}`,
+        },
+      ],
+    });
+    const response = data.choices[0].message.content;
+    //Sometimes some of the responses still include other info and I cant figure out why so this ensures that the response is JSON
+    const startIndex = response.indexOf("[");
+    const endIndex = response.lastIndexOf("]") + 1;
+    const json = response.slice(startIndex, endIndex);
 
-  res.send(JSON.parse(json));
+    res.send(JSON.parse(json));
+  } catch (error) {
+    res.status(400).send("Something went wrong. Please try again");
+  }
 });
 
 app.listen(PORT, () => {
