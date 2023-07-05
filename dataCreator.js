5c5844e07d124aa64f573924481481eed9644faf
personal <hasinmahmood01@gmail.com>
2023-07-05 18:10:54 +0100

    refactor:abstract openai api code to new folder

--- dataCreator.js
+++ dataCreator.js
@@ -1,10 +1,4 @@
-require("dotenv").config();
-const { Configuration, OpenAIApi } = require("openai");
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+const openai = require("./openai-api/openai.js");
 
 async function generateJSONObjects(object, numberOfObjects, extraInfo) {
   const obj = JSON.stringify(object);
