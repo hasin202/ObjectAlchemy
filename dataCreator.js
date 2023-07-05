0158c289413901ab961dd6ac33925397cf687ac9
personal <hasinmahmood01@gmail.com>
2023-07-05 18:01:25 +0100

    refactor:abstract async function for post endpoint

---
+++ dataCreator.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function generateJSONObjects(object, numberOfObjects, extraInfo) {
+  const obj = JSON.stringify(object);
+
+  try {
+    const { data } = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: `As a skilled data creator, your task is to generate a valid array of n JSON objects based 
+                    on a given example schema. The value of each property in the input object should be relevant to its
+                    name. For instance, if a property is named "name," you could assign the value 'bob roberts,' and if
+                    another property is called "ID," it could be assigned the value '34532,' and so on. Please ensure 
+                    that you only include the JSON values in your output and no additional strings or information. If a
+                    property is an object with multiple child properties, generate the same number of child properties
+                    accordingly.`,
+        },
+        {
+          role: "user",
+          content: `object: ${obj} number of objects: ${numberOfObjects} extra context: ${extraInfo}. Note: Don't change any of the property names passed in with the schema`,
+        },
+      ],
+    });
+
+    // Extract the JSON array from the response
+    const json = data.choices[0].message.content.match(/\[.*\]/s);
+    return JSON.parse(json);
+  } catch (error) {
+    throw new Error("Something went wrong. Please try again.");
+  }
+}
+
+module.exports = generateJSONObjects;
--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-require("dotenv").config();
-const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
 const cors = require("cors");
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+const generateJSONObjects = require("./dataCreator");
 
 const app = express();
 const PORT = 3000;
@@ -15,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
   res.send(
     "Oops. Looks like you sent a GET request. Please try again with a POST request instead."
   );
@@ -23,38 +17,19 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
   const { object, number_of_objects, extra_info } = req.body;
-  obj = JSON.stringify(object);
 
   try {
-    const { data } = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content: `As a skilled data creator, your task is to generate a valid array of n JSON objects
-          based on a given example schema. The value of each property in the input object should
-          be relevant to its name. For instance, if a property is named "name," you could assign
-          the value 'bob roberts,' and if another property is called "ID," it could be assigned the value
-          '34532,' and so on. Please ensure that you only include the JSON values in your output and no
-          additional strings or information. If a property is an object with multiple child properties,
-          generate the same number of child properties accordingly.`,
-        },
-        {
-          role: "user",
-          content: `object: ${obj} number of objects: ${number_of_objects} extra context: ${extra_info}. 
-          Note: Don't change any of the property names passed in with the schema`,
-        },
-      ],
-    });
-    //.match is used to ensure that only JSON is returned as in the response the JSON is always between [....]
-    const json = data.choices[0].message.content.match(/\[.*\]/s);
-    console.log(prompt);
-    res.send(JSON.parse(json));
+    const jsonObjects = await generateJSONObjects(
+      object,
+      number_of_objects,
+      extra_info
+    );
+    res.json(jsonObjects);
   } catch (error) {
-    res.status(400).send("Something went wrong. Please try again");
+    res.status(400).send(error.message);
   }
 });
 
 app.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
+  console.log(`Listening on ${PORT}`);
 });
