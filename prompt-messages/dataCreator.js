2393db12be2d480d4dd41e6bcd6a1cc2c88f28ad
personal <hasinmahmood01@gmail.com>
2023-07-09 02:48:41 +0100

    refactor: update indentation

--- prompt-messages/dataCreator.js
+++ prompt-messages/dataCreator.js
@@ -5,82 +5,82 @@ const dataCreatorMessage = (object, numberOfObjects, extraInfo) => {
     {
       role: "system",
       content: `As a skilled data creator, your task is to generate a valid array of n JSON objects based 
-                      on a given example schema. The value of each property in the input object should be relevant to its
-                      name. For instance, if a property is named "name," you could assign the value 'bob roberts,' and if
-                      another property is called "ID," it could be assigned the value '34532,' and so on. Please ensure 
-                      that you only include the JSON values in your output and no additional strings or information. If a
-                      property is an object with multiple child properties, generate the same number of child properties
-                      accordingly. Wherver the value for a property is "Image" dont make any changes, just keep the value
-                      as "Image". For example, given the following
-                      input object:
-      
-                      {
-                        {
-                          "id": "uuid",
-                          "item_image":"Image",
-                          "date": "calendar date",
-                          "time": "time of day",
-                          "inCart": true,
-                          "itemName": "string",
-                          "price": 0.0,
-                          "quantity": 0,
-                          "tags": [
-                            "string",
-                            "string",
-                            "string"
-                          ]
-                        },
-                        "number_of_objects":"1",
-                        "extra_info":"Each object is an item in an online shopping cart"
-                      }
-                    
-                     You would return something like: 
-                     [
-                      {
-                          id: c182b196-ff4e-436b-8e9b-4a97923003f9,
-                          "item_image":"Image",
-                          date:  2023-06-22
-                          time: 19:23:27
-                          inCart: true
-                          itemName: "cat food"
-                          price: 6.50,
-                          quantity: 2
-                          tags: [
-                            "pet",
-                            "pet food",
-                            "furry friend",
-                          ]
-                        }
-                      ]
-                   
-                  Another example given a different object as input is:
-                   
+                on a given example schema. The value of each property in the input object should be relevant to its
+                name. For instance, if a property is named "name," you could assign the value 'bob roberts,' and if
+                another property is called "ID," it could be assigned the value '34532,' and so on. Please ensure 
+                that you only include the JSON values in your output and no additional strings or information. If a
+                property is an object with multiple child properties, generate the same number of child properties
+                accordingly. Wherver the value for a property is "Image" dont make any changes, just keep the value
+                as "Image". For example, given the following
+                input object:
+
+                {
                   {
-                    {
-                      "name": "String",
-                      "age":"Int"
-                    },
-                    "number_of_objects":"3",
-                    "extra_info":"Each object is representing information about a person"
+                    "id": "uuid",
+                    "item_image":"Image",
+                    "date": "calendar date",
+                    "time": "time of day",
+                    "inCart": true,
+                    "itemName": "string",
+                    "price": 0.0,
+                    "quantity": 0,
+                    "tags": [
+                      "string",
+                      "string",
+                      "string"
+                    ]
+                  },
+                  "number_of_objects":"1",
+                  "extra_info":"Each object is an item in an online shopping cart"
+                }
+              
+                You would return something like: 
+                [
+                {
+                    id: c182b196-ff4e-436b-8e9b-4a97923003f9,
+                    "item_image":"Image",
+                    date:  2023-06-22
+                    time: 19:23:27
+                    inCart: true
+                    itemName: "cat food"
+                    price: 6.50,
+                    quantity: 2
+                    tags: [
+                      "pet",
+                      "pet food",
+                      "furry friend",
+                    ]
                   }
+                ]
+              
+                Another example given a different object as input is:
+                  
+                {
+                  {
+                    "name": "String",
+                    "age":"Int"
+                  },
+                  "number_of_objects":"3",
+                  "extra_info":"Each object is representing information about a person"
+                }
 
-                 You would return something like this:
+                You would return something like this:
 
-                  [
-                    {
-                      "name":"Aaron Paul",
-                      "Age": 42
-                    },
-                    {
-                      "name:"Bryan Cranston",
-                      "Age":58,
-                    }
-                    {
-                      "name":"Walter white",
-                      "Age": 58
-                    }
-                  ]
-                   `,
+                [
+                  {
+                    "name":"Aaron Paul",
+                    "Age": 42
+                  },
+                  {
+                    "name:"Bryan Cranston",
+                    "Age":58,
+                  }
+                  {
+                    "name":"Walter white",
+                    "Age": 58
+                  }
+                ]
+                `,
     },
     {
       role: "user",
