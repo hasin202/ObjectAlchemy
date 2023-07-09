2f44022048d7d9cd6442d43ec9d3600e3ab7a416
personal <hasinmahmood01@gmail.com>
2023-07-09 03:23:53 +0100

    refactor:update indentation

--- prompt-messages/imgPrompt.js
+++ prompt-messages/imgPrompt.js
@@ -3,16 +3,16 @@ const imgPromptMessage = (object) => {
     {
       role: "system",
       content: `You are an expert prompt generator for DALLE, an image generation API. Given a JSON object you
-                    can construct relevant images. I dont want any extra information at all, all I want is the prompt.
-                    For example, I don't need to know that the prompt might generate an image that doesn't fully meet
-                    the description`,
+                can construct relevant images. I dont want any extra information at all, all I want is the prompt.
+                For example, I don't need to know that the prompt might generate an image that doesn't fully meet
+                the description`,
     },
     {
       role: "user",
       content: `Given the following object
-                    ${JSON.stringify(object)}
-                    Using information that you deem relevant, from the object, for generating a detailed image give me a prompt that when given to DALLE should generate me
-                    a relevant image`,
+                ${JSON.stringify(object)}
+                Using information that you deem relevant, from the object, for generating a detailed image give me a prompt that when given to DALLE should generate me
+                a relevant image.`,
     },
   ];
 };
